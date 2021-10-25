8063a397842bfb3528d9fa41d669a111ba38942a
MichaelPNZ <85676466+MichaelPNZ@users.noreply.github.com>
2021-10-25 23:33:54 +0300

    Initial Commit

---
+++ Home Work 2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 2.3
+//
+//  Created by Михаил Позялов on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
